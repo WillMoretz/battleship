fe630c5d2b8cdd91229e52cd7ef1d0f2f8caa4f3
Will Moretz <whmoretz@gmail.com>
2023-03-29 19:12:49 -0400

    created generate board from array function

--- src/index.js
+++ src/index.js
@@ -1,39 +1,39 @@
 import { displayGame, displaySetup } from "./display";
 import { gameBoard } from "./game";
-import { chooseSquare, computer } from "./computer";
+import { chooseSquare, randomShipArray } from "./computer";
 
-const gameInit = (() => {
-  const playerBoard = gameBoard();
-  const computerBoard = gameBoard();
+const playerBoard = gameBoard();
+const computerBoard = gameBoard();
 
-  function init(playerShips, computerShips) {
-    playerShips.forEach((ship) => {
-      let first = undefined;
-      let name = undefined;
-      let last = undefined;
-      ship.forEach((square) => {
-        if (name === undefined) {
-          name = square.name;
-          return;
-        }
-        if (first === undefined) first = { col: square.col, row: square.row };
-        last = { col: square.col, row: square.row };
+function createBoardFromArray(ships, board, boardType) {
+  ships.forEach((ship) => {
+    let first = undefined;
+    let name = undefined;
+    let last = undefined;
+    ship.forEach((square) => {
+      if (name === undefined) {
+        name = square.name;
+        return;
+      }
+      if (first === undefined) first = { col: square.col, row: square.row };
+      last = { col: square.col, row: square.row };
+      if (boardType === "player") {
         const element = document
           .querySelector(".player-board")
           .querySelector(`.${square.col}${square.row}`);
         element.classList.add("ship");
-      });
-      playerBoard.placeShip(first.col, last.col, first.row, last.row, name);
+      }
     });
-  }
+    board.placeShip(first.col, last.col, first.row, last.row, name);
+  });
+}
 
-  return { init, playerBoard, computerBoard };
-})();
-
-function advanceGame(col, row) {}
+function init(playerShips, computerShips) {
+  createBoardFromArray(randomShipArray(), playerBoard, "player");
+  createBoardFromArray(randomShipArray(), computerBoard, "computer");
+}
 
 function markSquare(board, col, row, boardType) {
-  console.log("here");
   board.receiveAttack(col, row);
   if (board.attacks[board.attacks.length - 1].attackHit) {
     document
@@ -49,50 +49,15 @@ function markSquare(board, col, row, boardType) {
 }
 
 function handleSquareClick(col, row) {
-  if (gameInit.computerBoard.isRepeatedAttack(col, row)) return;
-  markSquare(gameInit.computerBoard, col, row, "computer");
+  if (computerBoard.isRepeatedAttack(col, row)) return;
+  markSquare(computerBoard, col, row, "computer");
 
-  if (gameInit.computerBoard.allShipsSunk()) return;
-  const computerChoice = chooseSquare(gameInit.playerBoard);
-  markSquare(
-    gameInit.playerBoard,
-    computerChoice.col,
-    computerChoice.row,
-    "player"
-  );
+  if (computerBoard.allShipsSunk()) return;
+  const computerChoice = chooseSquare(playerBoard);
+  markSquare(playerBoard, computerChoice.col, computerChoice.row, "player");
 }
 
-const shipsArray = [
-  [{ name: "Patrol Boat" }, { col: "a", row: 1 }, { col: "a", row: 2 }],
-  [
-    { name: "Battleship" },
-    { col: "d", row: 9 },
-    { col: "e", row: 9 },
-    { col: "f", row: 9 },
-    { col: "g", row: 9 },
-  ],
-  [
-    { name: "Destroyer" },
-    { col: "j", row: 6 },
-    { col: "j", row: 7 },
-    { col: "j", row: 8 },
-  ],
-  [
-    { name: "Aircraft Carrier" },
-    { col: "c", row: 3 },
-    { col: "c", row: 4 },
-    { col: "c", row: 5 },
-    { col: "c", row: 6 },
-    { col: "c", row: 7 },
-  ],
-  [
-    { name: "Submarine" },
-    { col: "e", row: 7 },
-    { col: "f", row: 7 },
-    { col: "g", row: 7 },
-  ],
-];
 displayGame();
-gameInit.init(shipsArray);
+init();
 
 export default handleSquareClick;
